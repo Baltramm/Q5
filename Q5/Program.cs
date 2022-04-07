ef67fe0ed8f11e68c1519e0827d193bbc807d1c6
Nikita Bochkovskiy <nikita.bochkovsky@yandex.ru>
2022-04-07 20:48:20 +0600

    Добавьте файлы проекта.

---
+++ Q5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Q5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (string Name,string []Dishes) User;
+            Console.WriteLine("Введите имя");
+            User.Name = Console.ReadLine();
+            User.Dishes = new string[5];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Ваше любимое блюдо номер{0}:",i+1);
+                User.Dishes[i] = Console.ReadLine();
+            }
+            Console.Write(User.Dishes);
+            Console.WriteLine(User.Name);
+        }
+    }
+}
